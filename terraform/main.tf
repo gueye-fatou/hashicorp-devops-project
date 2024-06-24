8929a7c5be6baa55b4544eccfb4c88fbef2d2b59
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2024-06-24 03:32:52 +0000

    Add Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,69 @@
-"" 
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet.id
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Previous installation commands
+      "sudo yum install -y yum-utils shadow-utils",
+      "sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo",
+      "sudo yum -y install vault consul python3-pip",
+      "pip3 install flask hvac",
+      # Run the Vault setup script
+      "bash /hashicorp-devops-project/vault/setup_vault.sh",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+}
+
