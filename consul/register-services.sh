66de81fdd6fa4d2134293db5412e8dfb029d416f
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2024-06-24 03:36:55 +0000

    Configure the script to Start Consul and Register the Service

--- consul/register-services.sh
+++ consul/register-services.sh
@@ -1,1 +1,19 @@
-"" 
+#!/bin/bash
+
+# Start Consul in the background
+consul agent -dev -bind 127.0.0.1 &
+
+# Create a Consul service definition file for the Flask app
+cat <<EOF > /etc/consul.d/web.json
+{
+  "service": {
+    "name": "web",
+    "tags": ["web"],
+    "port": 80
+  }
+}
+EOF
+
+# Register the web service
+consul services register /etc/consul.d/web.json
+ 
