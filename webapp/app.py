12adb2ab33444bc26fd1366d3099fbaa03881a9b
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2024-06-24 03:38:12 +0000

    Modify app to retrieve the secrets from Vault and register itself with Consul

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,33 @@
 from flask import Flask
+import hvac
+import requests
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+# Vault client
+client = hvac.Client(url='http://127.0.0.1:8200')
+client.token = 'root'
+secret = client.secrets.kv.v2.read_secret_version(path='webapp/config')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+username = secret['data']['data']['username']
+password = secret['data']['data']['password']
+
+@app.route('/')
+def index():
+    return f"Username: {username}, Password: {password}"
+
+def register_service():
+    # Register service with Consul
+    service_payload = {
+        "Name": "web",
+        "Tags": ["web"],
+        "Port": 80,
+        "Address": "127.0.0.1"
+    }
+    requests.put("http://127.0.0.1:8500/v1/agent/service/register", data=json.dumps(service_payload))
+
+if __name__ == '__main__':
+    register_service()
+    app.run(host='0.0.0.0', port=80)
 
