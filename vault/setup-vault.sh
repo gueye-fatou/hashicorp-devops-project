5e0f5f332a06f7976a5560012adc4955b304ad3b
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2024-06-24 03:35:43 +0000

    Configue script to Start Vault and Initialize Secrets

--- vault/setup-vault.sh
+++ vault/setup-vault.sh
@@ -1,1 +1,16 @@
-"" 
+#!/bin/bash
+
+# Start Vault in the background
+vault server -dev -dev-root-token-id="root" &
+sleep 5
+
+# Initialize and unseal Vault
+export VAULT_ADDR='http://127.0.0.1:8200'
+vault login root
+
+# Enable KV secrets engine
+vault secrets enable -version=2 kv
+
+# Add a test secret
+vault kv put kv/webapp/config username="admin" password="admin123"
+ 
